ae1e5b5e2ce6625742e387395e01704e4fa9a343
Beatriz <bia_nonato@icloud.com>
2018-09-29 18:22:29 -0300

    corrigir tratamento de entrada

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-// let nome = prompt("Digite aqui seu nome");
+let entrada = prompt("Digite um número:");
 
-// console.log(`O nome digitado foi ${nome}`);
-
-// alert(`Olá! Seja bem vindo, ${nome}!!!`);   (``: template string) ("": just string)
-
-let numero = Number(prompt("Digite um número:"));
-
-if(entrada.trim()){ //.trim é tratamento de entrada: começa e termina com caractere válido
-    let numero = Number=(entrada);
+if(entrada){ //.trim é tratamento de entrada: começa e termina com caractere válido
+    let numero = Number(entrada.trim());
     if(numero > 15){
-        console.log("Número maior que quinze.")
+        console.log("Número maior que 15.")
     }
     else if(numero < 15){
-        console.log("Número menor que quinze.")
+        console.log("Número menor que 15.")
     }
     else if(numero === 15){
         console.log("Número igual a 15.")
